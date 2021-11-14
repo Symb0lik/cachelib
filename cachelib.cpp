a03b697b765386edf87f842b7dc5357daf548438
Symb0lik <94119817+Symb0lik@users.noreply.github.com>
2021-11-14 23:04:42 +0300

    Add project files.

---
+++ cachelib.cpp
@@ -0,0 +1,12 @@
+﻿// cachelib.cpp : Defines the entry point for the application.
+//
+
+#include "cachelib.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
